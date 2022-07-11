841c8159b33e5465ae618e18846bd24c92cde2d0
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-07-11 13:28:08 -0300

    Add files via upload

---
+++ primeiro-codigo-java/primeiro-codigo-intellij/src/SCG/PrimeiroCodigoIntelli.java
@@ -0,0 +1,14 @@
+package SCG;
+
+import SCG.Gato;
+
+public class PrimeiroCodigoIntelli {
+    public static void main(String[] args) {
+
+        System.out.println("Oi gente!");
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+}
