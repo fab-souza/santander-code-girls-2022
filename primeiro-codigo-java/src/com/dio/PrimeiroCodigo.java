5aa0b58e828f0734def3a638f0009de922c91345
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-07-10 21:52:45 -0300

    Aula sobre o Eclipse

---
+++ primeiro-codigo-java/src/com/dio/PrimeiroCodigo.java
@@ -0,0 +1,16 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroCodigo {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		System.out.println("Oi gente!" + (a+b));
+		
+		Gato gato = new Gato();
+		System.out.println(gato);
+	}
+
+}
