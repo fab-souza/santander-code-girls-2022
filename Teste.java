d312ff525380a8a760161286e3f2e4b655ee4cb1
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-07-17 20:07:42 -0300

    Desafio módulo II

--- Teste.java
+++ Teste.java
@@ -6,13 +6,13 @@ public class Teste {
         Scanner leitor = new Scanner(System.in);
         double valor = Double.parseDouble(leitor.nextLine());
         if (valor >= 0 && valor <= 25.000000) {
-            System.out.println("Entre [0,25]");
+            System.out.println("Intervalo [0,25]");
         } else if (valor > 25.000001 && valor <= 50.000000) {
-            System.out.println("Entre (25,50]");
+            System.out.println("Intervalo (25,50]");
         } else if (valor > 50.000001 && valor <= 75.000000) {
-            System.out.println("Entre (50,75]");  
+            System.out.println("Intervalo (50,75]");  
         } else if (valor > 75.000001 && valor <= 100.000000) {
-            System.out.println("Entre (75,100]");
+            System.out.println("Intervalo (75,100]");
         } else {
             System.out.println("Fora de intervalo");
         }
