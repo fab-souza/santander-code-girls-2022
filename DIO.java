ad93413297a943ce4e67534fd7864886ec4deee5
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-07-17 19:26:41 -0300

    Desafio módulo II

---
+++ DIO.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DIO {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+	  int n = leitor.nextInt();
+   System.out.println((  10 ) * (  n  )   / n   );
+    	
+	}
+    }
